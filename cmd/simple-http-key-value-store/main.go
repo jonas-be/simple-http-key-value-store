6fc025db04e4f610c08cf06e97de9ad496abe0f8
jonas-be <jonasbender2005@gmail.com>
2023-03-10 15:16:47 +0100

    Implement logging middleware

--- cmd/simple-http-key-value-store/main.go
+++ cmd/simple-http-key-value-store/main.go
@@ -4,11 +4,14 @@ import (
 	"net/http"
 	"simple-http-key-value-store/internal/database"
 	"simple-http-key-value-store/internal/endpoint"
+	"simple-http-key-value-store/internal/middelware"
 )
 
-var db = endpoint.DataHandler{Db: &database.Database{}}
+var db = endpoint.DataHandler{Db: &database.Database{Data: map[string]string{}}}
 
 func main() {
-	http.HandleFunc("/data", db.HandleRequest)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/data", db.HandleRequest)
+	http.ListenAndServe(":8080", middelware.LoggingMiddleware(mux))
 }
---
+++ internal/middelware/logging.go
@@ -0,0 +1,48 @@
+package middelware
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w}
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+
+	return
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wrapped := wrapResponseWriter(w)
+		next.ServeHTTP(wrapped, r)
+		fmt.Printf(
+			"%s %s %d %vms\n",
+			r.Method,
+			r.RequestURI,
+			wrapped.status,
+			time.Since(start).Milliseconds(),
+		)
+	})
+}
